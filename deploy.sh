29d501ddfdac26777cbdd2455cc712aafe9b4e06
Beto Muniz <contato@betomuniz.com>
2019-04-27 17:11:40 -0300

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,14 @@ cd /var/www/betomuniz.com/
 git checkout --force master
 git pull origin master
 
+echo '2. Stop server process'
+pm2 stop betomuniz
+
 echo "3. Install dependencies and build"
 yarn install && yarn build
+
+echo '4. Restart server process'
+pm2 restart betomuniz
 
 echo 'Done!'
 
