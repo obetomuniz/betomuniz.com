dbed88bb5a071e6d8f7d4c3b94354988687dd116
Beto Muniz <contato@betomuniz.com>
2020-07-14 00:38:49 -0300

    Add glitch on logo

--- src/components/header/ui.js
+++ src/components/header/ui.js
@@ -1,5 +1,195 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import { Link } from "gatsby"
+
+const glitch1 = keyframes`
+  0% {
+    transform: none;
+    opacity: 1;
+  }
+  7% {
+    transform: skew(-0.5deg, -0.9deg);
+    opacity: 0.75;
+  }
+  10% {
+    transform: none;
+    opacity: 1;
+  }
+  27% {
+    transform: none;
+    opacity: 1;
+  }
+  30% {
+    transform: skew(0.8deg, -0.1deg);
+    opacity: 0.75;
+  }
+  35% {
+    transform: none;
+    opacity: 1;
+  }
+  52% {
+    transform: none;
+    opacity: 1;
+  }
+  55% {
+    transform: skew(-1deg, 0.2deg);
+    opacity: 0.75;
+  }
+  50% {
+    transform: none;
+    opacity: 1;
+  }
+  72% {
+    transform: none;
+    opacity: 1;
+  }
+  75% {
+    transform: skew(0.4deg, 1deg);
+    opacity: 0.75;
+  }
+  80% {
+    transform: none;
+    opacity: 1;
+  }
+  100% {
+    transform: none;
+    opacity: 1;
+  }
+`
+
+const glitch2 = keyframes`
+  0% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  7% {
+    filter: hue-rotate(300deg);
+    transform: translate3d(-2px, -3px, 0);
+    opacity: 0.5;
+  }
+  10% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  27% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  30% {
+    filter: hue-rotate(300deg);
+    transform: translate3d(-5px, -2px, 0);
+    opacity: 0.5;
+  }
+  35% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  52% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  55% {
+    filter: hue-rotate(300deg);
+    transform: translate3d(-5px, -1px, 0);
+    opacity: 0.5;
+  }
+  50% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  72% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  75% {
+    filter: hue-rotate(300deg);
+    transform: translate3d(-2px, -6px, 0);
+    opacity: 0.5;
+  }
+  80% {
+    transform: none;
+    opacity: 0;
+  }
+  100% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+`
+
+const glitch3 = keyframes`
+  0% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  7% {
+    filter: hue-rotate(150deg);
+    transform: translate3d(2px, 3px, 0);
+    opacity: 0.5;
+  }
+  10% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  27% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  30% {
+    filter: hue-rotate(150deg);
+    transform: translate3d(5px, 2px, 0);
+    opacity: 0.5;
+  }
+  35% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  52% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  55% {
+    filter: hue-rotate(150deg);
+    transform: translate3d(5px, 1px, 0);
+    opacity: 0.5;
+  }
+  50% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  72% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  75% {
+    filter: hue-rotate(150deg);
+    transform: translate3d(2px, 6px, 0);
+    opacity: 0.5;
+  }
+  80% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+  100% {
+    filter: hue-rotate(0deg);
+    transform: none;
+    opacity: 0;
+  }
+`
 
 export const Container = styled.header`
   display: flex;
@@ -18,34 +208,49 @@ export const Container = styled.header`
   }
 `
 
-export const Photo = styled(Link)`
-  height: 80px;
-  overflow: hidden;
-  width: 80px;
-  display: block;
-  border-radius: 100%;
-  overflow: hidden;
-  margin-bottom: var(--DEFAULT_MARGIN);
-  border: 2px solid var(--DEFAULT_HEADER_PHOTO_BORDER_COLOR);
-  box-shadow: 0 10px 6px -8px var(--DEFAULT_BOX_SHADOW_COLOR);
-
-  @media screen and (max-width: 440px) {
-    margin-bottom: calc(var(--DEFAULT_MARGIN_MOBILE) / 1.5);
-  }
-`
-
 export const PhotoImage = styled.img`
   height: 100%;
   width: 100%;
   border-radius: 100%;
   overflow: hidden;
-  transform: scale3d(1, 1, 1);
-  filter: hue-rotate(300deg);
-  transition: all 0.5s ease-in;
+  animation: ${glitch1} 10s infinite linear;
+  border: 2px solid var(--DEFAULT_HEADER_PHOTO_BORDER_COLOR);
+  z-index: 2;
+`
 
-  &:hover,
-  &:focus {
-    filter: hue-rotate(0);
+export const Photo = styled(Link)`
+  height: 80px;
+  width: 80px;
+  display: block;
+  border-radius: 100%;
+  position: relative;
+  margin-bottom: var(--DEFAULT_MARGIN);
+  box-shadow: 0 10px 6px -8px var(--DEFAULT_BOX_SHADOW_COLOR);
+
+  &::before,
+  &::after {
+    background-image: url("https://www.gravatar.com/avatar/fff7258836f20ea66b061b49a51fe8dd?s=170");
+    background-size: cover;
+    animation: ${glitch2} 10s infinite linear;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    height: 100%;
+    width: 100%;
+    display: block;
+    border-radius: 100%;
+    border: 2px solid var(--DEFAULT_HEADER_PHOTO_BORDER_COLOR);
+    z-index: 2;
+    content: "";
+  }
+
+  &::after {
+    animation: ${glitch3} 10s infinite linear;
+  }
+
+  @media screen and (max-width: 440px) {
+    margin-bottom: calc(var(--DEFAULT_MARGIN_MOBILE) / 1.5);
   }
 `
 
