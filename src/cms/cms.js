783812e1cda89b2cc8b160cfd6e73c11d351b8ff
Beto Muniz <contato@betomuniz.com>
2020-07-26 19:00:20 -0300

    Add cloudinary support

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,7 @@
 import React from "react"
 import CMS from "netlify-cms-app"
 import { MdxControl, setupPreview } from "netlify-cms-widget-mdx"
-
-// import uploadcare from 'netlify-cms-media-library-uploadcare'
-// import cloudinary from 'netlify-cms-media-library-cloudinary'
+import cloudinary from "netlify-cms-media-library-cloudinary"
 
 import Code from "../components/code/code"
 import { Drop, A, Video, Tweet } from "../components/mdx"
@@ -11,8 +9,7 @@ import withStyledComponents from "./hooks/withStyledComponents"
 import PostPreview from "./preview-templates/post-preview"
 import DropPreview from "./preview-templates/drop-preview"
 
-// CMS.registerMediaLibrary(uploadcare);
-// CMS.registerMediaLibrary(cloudinary);
+CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate("blog", withStyledComponents(PostPreview))
 CMS.registerPreviewTemplate("drops", withStyledComponents(DropPreview))
