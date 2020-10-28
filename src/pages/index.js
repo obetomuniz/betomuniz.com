2a24efaf5bb7e6e30ded8a62f442d792441b976e
Beto Muniz <contato@betomuniz.com>
2020-10-28 14:20:01 -0300

    Improve code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,13 @@
 import { NextSeo } from "next-seo";
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown/with-html";
-import styled from "styled-components";
-import { rem } from "polished";
 
+import {
+  SocialsContainer,
+  About,
+  NewsletterContainer,
+} from "../styles/pages/home";
 import { Socials, Layout, Newsletter } from "../components";
-
-const SocialsContainer = styled.div`
-  width: 550px;
-  margin: var(--HORIZONTAL_MARGIN) auto 0;
-  display: flex;
-  justify-content: center;
-
-  @media screen and (max-width: 550px) {
-    margin: var(--HORIZONTAL_MARGIN_MOBILE) auto 0;
-    width: 100%;
-  }
-`;
-
-const About = styled.main`
-  text-align: center;
-  width: 550px;
-  margin: 0 auto;
-  p {
-    font-size: ${rem("18px")};
-    line-height: ${rem("44px")};
-
-    &:first-child {
-      &:first-letter {
-        font-size: ${rem("32px")};
-      }
-    }
-  }
-
-  a {
-    font-weight: bold;
-  }
-
-  @media screen and (max-width: 550px) {
-    width: 100%;
-
-    p {
-      font-size: ${rem("16px")};
-      line-height: ${rem("30px")};
-    }
-  }
-`;
-
-const NewsletterContainer = styled.div`
-  width: 550px;
-  margin: 0 auto var(--HORIZONTAL_MARGIN);
-
-  @media screen and (max-width: 550px) {
-    margin: 0 auto var(--HORIZONTAL_MARGIN_MOBILE);
-    width: 100%;
-  }
-`;
 
 const Home = (props) => {
   const {
